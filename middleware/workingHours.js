9ee1722140224deeb34db320266593a9d9c78772
Ngumonelson123 <your-email@example.com>
2026-01-13 23:20:21 +0300

    add middleware

--- middleware/workingHours.js
+++ middleware/workingHours.js
@@ -1,17 +1,18 @@
-const {DateTime } = require("luxon");
+const { DateTime } = require("luxon");
 
-module.exports = function workingHours(req, resizeBy, next) {
-    const now = DateTime.now().setZone("Africa/Nairobi");
-    
-    const isWeekday = now.weekday >= 1 && now.weekday <= 5; //1=Mon....5=Fri
-    const hour = now.hour; //0-23
+module.exports = function workingHours(req, res, next) {
+  const now = DateTime.now().setZone("Africa/Nairobi");
 
-    //working hours 09:00 <= time < 17:00
-    const isWorkingHours = hour >= 9 && hour < 17;
-    if (isWeekend && isWorkingHours) return next();
+  // Mon=1 ... Sun=7
+  const isWeekday = now.weekday >= 1 && now.weekday <= 5;
 
-    return resizeBy.status(403).render("closed" , {
-        title: "Closed",
-        now: now.toFormat("cccc, dd LLL yyyy - HH:mm")
-    });
+  // Working hours: 09:00 <= time < 17:00
+  const isWorkingHours = now.hour >= 9 && now.hour < 17;
+
+  if (isWeekday && isWorkingHours) return next();
+
+  return res.status(403).render("closed", {
+    title: "Closed",
+    now: now.toFormat("cccc, dd LLL yyyy - HH:mm"),
+  });
 };
